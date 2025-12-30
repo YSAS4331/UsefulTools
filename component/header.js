cb3dae86dabbb096ab6f4d1b031d5895652bce82
YSAS4331 <ryo.matsu0619@gmail.com>
2025-12-31 08:56:12 +0900

    Update header.js

--- component/header.js
+++ component/header.js
@@ -105,7 +105,7 @@ class Header extends HTMLElement {
         <button id="settings" title="設定を開く" class="nostyle">
           <i class="fa-solid fa-user-gear"></i>
         </button>
-        <button id="colorTheme" title="カラーテーマを変更" style="color: var(--accentBlack);">
+        <button id="colorTheme" title="カラーテーマを変更" style="color: var(--accentBlack); border: 2px solid white;">
           <i class="fa-solid fa-palette"></i>
         </button>
       </div>
