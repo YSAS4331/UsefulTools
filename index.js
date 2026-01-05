d9de8188dda2b5a4a2fc60adbc5ee9de45a278c9
YSAS4331 <ryo.matsu0619@gmail.com>
2026-01-05 12:23:12 +0000

    Update files

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 import 'https://ysas4331.github.io/UsefulTools/component/header.js';
+import 'https://ysas4331.github.io/UsefulTools/component/navigation.js';
 import 'https://ysas4331.github.io/UsefulTools/component/accor.js';
