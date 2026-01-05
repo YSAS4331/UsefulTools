ec41956704db4303749eb3d8b5ed52d18807b23f
YSAS4331 <ryo.matsu0619@gmail.com>
2026-01-05 09:55:32 +0900

    Update cookie.js

--- component/cookie.js
+++ component/cookie.js
@@ -1,1 +1,12 @@
+(() => {
+  const state = localStorage.getItem('USEFUL_cookie');
+  if (state !== null) return;
 
+  const c = e => document.createElement(e);
+
+  const parent = c('div');
+
+  parent.innerHTML = `
+    <span class="material-symbols-outlined">cookie</span>
+  `;
+})();
