c14da210bb0174909b43e9ae5a1ff697de6936dc
YSAS4331 <ryo.matsu0619@gmail.com>
2026-01-10 10:57:15 +0900

    Create utility.js

---
+++ fncs/utility.js
@@ -0,0 +1,38 @@
+function $(s,r=document) {
+  return r.querySelectorAll(s);
+}
+$.$ = (s,r=document) => [...r.querySelectorAll(s)];
+$.create = e => document.createElement(e);
+$.dialog = (display='flex') => {
+  const bg = $.create('div');
+  const dialogDiv = $.create('div');
+
+  Object.assign(bg.style, {
+    position: 'fixed',
+    left: '0',
+    top: '0',
+    width: '100vw',
+    height: '100vh',
+    background: 'rgba(0,0,0,0.75)',
+    zIndex: '9999'
+  });
+  Object.assign(dialogDiv.style, {
+    display: display,
+    borderRadius: 'var(--radius)',
+    border: '2px var(--accent) solid',
+    zIndex: '10000',
+    position: 'fixed',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%,-50%)'
+  });
+
+  bg.appendChild(dialogDiv);
+  document.body.appendChild(bg);
+
+  function remove() {
+    bg.remove();
+  }
+  
+  return { dialogDiv, remove };
+}
