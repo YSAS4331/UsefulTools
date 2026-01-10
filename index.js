b4cfdc57fbe35e3471f7bd5da9b3da7c376a93b8
YSAS4331 <ryo.matsu0619@gmail.com>
2026-01-10 18:46:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import 'https://ysas4331.github.io/UsefulTools/component/header.js';
 import 'https://ysas4331.github.io/UsefulTools/component/navigation.js';
 import 'https://ysas4331.github.io/UsefulTools/component/accor.js';
+import 'https://ysas4331.github.io/UsefulTools/fncs/settings.js';
