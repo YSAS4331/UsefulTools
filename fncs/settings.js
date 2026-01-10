aa36b7431c90a97a4c23c3e44c6f0d0ab95ae05e
YSAS4331 <ryo.matsu0619@gmail.com>
2026-01-10 18:50:34 +0900

    Update settings.js

--- fncs/settings.js
+++ fncs/settings.js
@@ -150,5 +150,5 @@ window.addEventListener('open-settings', async () => {
   const { local, indexed, localSize, indexedSize } = await getAllUsefulData();
 
   $('.localStorageSize', main).textContent = `localStorage: ${(localSize / 1024).toFixed(2)} KB / ${local.length} items`;
-  $('.indexed', main).textContent = `indexedDB: ${(indexedSize / 1024).toFixed(2)} KB / ${indexed.length} DBs`;
+  $('.indexedSize', main).textContent = `indexedDB: ${(indexedSize / 1024).toFixed(2)} KB / ${indexed.length} DBs`;
 });
