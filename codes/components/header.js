f7a067df16d3dfd2342c9fc1a0bcf3f6ef523b9c
YSAS4331 <ryo.matsu0619@gmail.com>
2026-07-13 20:13:18 +0900

    Update header.js

--- codes/components/header.js
+++ codes/components/header.js
@@ -1,1 +1,21 @@
+class header extends HTMLElement {
+  constructor() {
+    super();
+    
+    this.attachShadow({ mode: 'open' });
+  }
+  
+  #render() {
+    this.innerHTML = `
+<header>
+  <a href="/UsefulTools/">UsefulTools</a>
+</header>
+    `;
+  }
+  
+  connectedCallback() {
+    this.#render();
+  }
+}
 
+customElements.define('app-header', header);
