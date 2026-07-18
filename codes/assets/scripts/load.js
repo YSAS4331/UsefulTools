4c41d740802385fe50a714efaaacc2c254c236fd
YSAS4331 <ryo.matsu0619@gmail.com>
2026-07-19 08:17:33 +0900

    Update load.js

--- codes/assets/scripts/load.js
+++ codes/assets/scripts/load.js
@@ -1,2 +1,6 @@
 import '/UsefulTools/components/header.js';
 import '/UsefulTools/components/sidebar.js';
+import { createIcons, Star } from 'https://cdn.jsdelivr.net/npm/lucide@1.25.0/+esm';
+createIcons({
+  Star
+});
