8f3fc2f12e0405a75f41e5ae13a50922597a53b7
YSAS4331 <ryo.matsu0619@gmail.com>
2026-07-19 19:24:01 +0900

    Update tools.js

--- codes/assets/scripts/tools.js
+++ codes/assets/scripts/tools.js
@@ -1,3 +1,28 @@
-const init = () => {};
+const $ = (s,r=document) => r.querySelector(s);
+const shareData = {
+  title: document.title,
+  text: $('meta[name="description"]')?.content ?? '',
+  url: location.href
+};
+
+const init = () => {
+  const buttonsParent = $('tool-buttons');
+  const shareBtn = $('tool-share', buttonsParent);
+  const favoBtn = $('tool-favorite', buttonsParent);
+
+  shareBtn.addEventListener('click', async () => {
+    try {
+      if (navigator.share) {
+        await navigator.share(shareData);
+      } else {
+        console.log('お使いのデバイスは、共有をサポートしていません。');
+      }
+    } catch(e) {
+      if (e.name !== 'AbortError') {
+        console.warn('共有に失敗しました。', e);
+      }
+    }
+  });
+};
 
 export { init };
