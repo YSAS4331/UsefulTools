eb8e27fc4de47c9aca5cbf067244222373ff6da0
YSAS4331 <ryo.matsu0619@gmail.com>
2026-07-19 10:17:20 +0900

    Update load.js

--- codes/assets/scripts/load.js
+++ codes/assets/scripts/load.js
@@ -1,13 +1,14 @@
 import '/UsefulTools/components/header.js';
 import '/UsefulTools/components/sidebar.js';
-import { createIcons, Star, Folders, Blocks } from 'https://cdn.jsdelivr.net/npm/lucide@1.25.0/+esm';
+import { createIcons, Star, Folders, Blocks, Info } from 'https://cdn.jsdelivr.net/npm/lucide@1.25.0/+esm';
 
 const setupIcons = () => {
   createIcons({
     icons: {
       Star,
       Folders,
-      Blocks
+      Blocks,
+      Info
     }
   });
 }
