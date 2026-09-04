c0ec47212f9ea6e045e58b30045cf4606b70ba33
YSAS4331 <ryo.matsu0619@gmail.com>
2026-09-05 02:18:42 +0900

    Update load.js

--- codes/assets/scripts/load.js
+++ codes/assets/scripts/load.js
@@ -1,5 +1,7 @@
 import "/UsefulTools/components/header.js";
 import "/UsefulTools/components/sidebar.js";
+import '/Components/source/tabs.js';
+
 import {
   createIcons, Star, Folders, Blocks, Info, Hash, Share2, 
   HeartPlus, BookOpen, Lightbulb, AudioLines, Maximize2, Minimize2
