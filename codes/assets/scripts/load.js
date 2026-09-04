22663ce8dd0decb8d9eb3dbbb815dee1291f2144
YSAS4331 <ryo.matsu0619@gmail.com>
2026-09-04 22:21:40 +0900

    Update load.js

--- codes/assets/scripts/load.js
+++ codes/assets/scripts/load.js
@@ -2,6 +2,8 @@ import '/UsefulTools/components/header.js';
 import '/UsefulTools/components/sidebar.js';
 import { createIcons, Star, Folders, Blocks, Info, Hash, Share2, HeartPlus, BookOpen, Lightbulb, AudioLines, Maximize2, Minimize2 } from 'https://cdn.jsdelivr.net/npm/lucide@1.25.0/+esm';
 
+const lucide = { createIcons, Star, Folders, Blocks, Info, Hash, Share2, HeartPlus, BookOpen, Lightbulb, AudioLines, Maximize2, Minimize2 };
+
 const setupIcons = () => {
   createIcons({
     icons: {
@@ -28,3 +30,5 @@ window.addEventListener('spa:router', e => {
 });
 
 setupIcons();
+
+window.spaRouter.commonStorage.set("lucide", lucide);
